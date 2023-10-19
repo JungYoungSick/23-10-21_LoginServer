ea8133deecb31b576b3034a0ae43aafca37a1886
JungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-19 23:08:36 +0900

    Server 작업 현 상태 주석처리

--- Server.js
+++ Server.js
@@ -1,13 +1,15 @@
+//경로 변수 작성.
 const http = require('http')
 const fs = require('fs')
 const qureyString = require('querystring')
 
+// HTTP모듈과 콜백을 사용한 서버 생성.
 const server = http.createServer((req, res) => {
   function serverErrerLog() {
     res.writeHead(500)
     return res.end("서버가 안돼요! 수정하세요");
   }
-
+  //? join.html 페이지 활성화를 위한 GET방식 페이지 가져오기[구동 확인]
   if(req.url === '/' && req.method === 'GET') {
     fs.readFile('./static/onepage/join.html',(err,data) => {
       if(err) {
@@ -17,6 +19,7 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   }
+  //? join.css 페이지 활성화를 위한 GET방식 페이지 가져오기[구동 확인]
   else if(req.url ==='/join.css' && req.method === 'GET') {
     fs.readFile('./static/onepage/join.CSS',(err,data) => {
       if(err) {
@@ -25,6 +28,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, {'Content-Type':'text/css;', 'charset':'utf-8'});
       res.end(data)
     });
+  //? join.js 페이지 활성화를 위한 GET방식 페이지 가져오기[구동 확인] 
   }else if (req.url === '/join.js' && req.method === 'GET') {
     fs.readFile('./static/onepage/join.js', (err,data) => {
       if(err) {
@@ -33,16 +37,19 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, {'content-type':'application/javascript', 'charset':'utf-8'});
       res.end(data)
     }); 
+    //! Titlepage.html 페이지 활성화를 위한 post방식 페이지 불러오기[구동 실패]
     }else if(req.url === './inpo.html' && req.method === 'post') {
       let none = "";
       req.on('data', (chenk) => {
         none += chenk.toString();
       })
+      //! 아이디 작성 시 값 추가 작동 확인이 어려움.
       req.on('end', ()=> {
         const queryParseNone=qureyString.parse(none);
         const {UserID} = queryParseNone
         console.log(UserID);
       })
+      //! Titlepage.html 페이지 활성화를 위한 post방식 페이지 가져오기[구동 실패]
       fs.readFile('./static/twopage/Titlepage.html', (err, data) => {
         if(err) {
           console.log(err)
